b65c84cf5215db9aa039407fbb9c8f833cf2d0a5
Kyle-Hess <kyle.hess@my.jcu.edu.au>
2016-04-03 16:54:20 +1000

    adding items (in progress)

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
         elif choice == "R":
             print("return item")
         elif choice == "A":
-            print("add an item")
+            add_item()
         else:
             print("Invalid choice.")
         print(MENU)
@@ -30,15 +30,34 @@ def main():
 ########
 
 def loading_items():
+    """""
     items = open("items.csv", "r")
     for line in items.readlines():
         print("! ", line.strip())
     items.close()
-
+"""""
+    items = open("items.csv", "r")
+    reader = csv.reader(items, delimiter="\t")
+    for i, line in enumerate(reader):
+        print('{} = {}'.format(i, line))
+    items.close()
 
 #######
 def hiring_item():
     return
 
+def add_item():
+    new = []
 
+    name = input("name of item: ")
+    new.append(name)
+    description = input("enter description: ")
+    new.append(description)
+    price = input("enter price: ")
+    new.append(price)
+    new.append('in')
+    print(new)
+    add = open("items.csv", "a")
+    add.write(','.join(new))
+    add.close()
 main()
