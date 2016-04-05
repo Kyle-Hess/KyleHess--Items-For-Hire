65f2d37fbdb0dd4379c15d450480488cabe07ec7
Kyle-Hess <kyle.hess@my.jcu.edu.au>
2016-04-05 22:24:09 +1000

    tuples.....

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def main():
         if choice == "L":
             loading_items()
         elif choice == "H":
-            print("Hire an Item")
+            hiring_item()
         elif choice == "R":
             print("return item")
         elif choice == "A":
@@ -36,28 +36,65 @@ def loading_items():
         print("! ", line.strip())
     items.close()
 """""
+    items = open("items.csv", "r+")
+    wr = csv.writer(items, dialect='excel')
+
+
+    for line in items:
+        match = line.strip().split(",")
+        mylist = []
+
+        name = match[0]
+        description = match[1]
+        price = match[2]
+        hire = match[3]
+        mylist.append((match[0], match[1], match[2], match[3]))
+
+        for thing in mylist:
+            if len(thing[3]) != 3:
+                print(thing[0:3])
+    items.close()
+"""
+            for i, lines in enumerate(mylist):
+                print('{} = {}'.format(i, lines))
+                i += 1
+
+
+    items = open("items.csv", "r")
+    reader = csv.reader(items, delimiter="\t")
+    for i, line in enumerate(reader):
+        print('{} = {}'.format(i, line).split())
+    items.close()
+"""
+#######
+def hiring_item():
     items = open("items.csv", "r")
     reader = csv.reader(items, delimiter="\t")
     for i, line in enumerate(reader):
         print('{} = {}'.format(i, line))
     items.close()
 
-#######
-def hiring_item():
-    return
 
 def add_item():
     new = []
 
-    name = input("name of item: ")
-    new.append(name)
+    itemName = input("name of item: ")
+    new.append(itemName)
     description = input("enter description: ")
     new.append(description)
     price = input("enter price: ")
     new.append(price)
     new.append('in')
     print(new)
+
+    with open("output.csv", "a") as csvfile:
+        wr = csv.writer(csvfile, dialect='excel')
+        wr.writerow(new)
+
+"""
     add = open("items.csv", "a")
     add.write(','.join(new))
     add.close()
+"""
+
 main()
