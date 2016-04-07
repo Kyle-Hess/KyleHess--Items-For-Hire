f4c32ad2b03574b8b26586286f58829955aac74c
Kyle-Hess <kyle.hess@my.jcu.edu.au>
2016-04-07 15:33:05 +1000

    Hires and returns items. (Only by name of item)

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import csv
 
 MENU = "\nMenu:\n(L)ist all items\n(H)ire an item\n(R)eturn an item\n(A)dd new item\n(Q)uit"
+mylist = []
+
 
 def main():
     print("item for hire by Kyle Hess")
+    load_items()
     print(MENU)
 
-    file = open("items.csv", "r")
-
+    # items = open("items.csv", "r")
+    # wr = csv.writer(items, dialect='excel')
     choice = input("Enter Choice: ").upper()
     while choice != "Q":
-
         if choice == "L":
-            loading_items()
+            listing_items()
         elif choice == "H":
             hiring_item()
         elif choice == "R":
-            print("return item")
+            return_item()
         elif choice == "A":
             add_item()
         else:
@@ -25,76 +27,75 @@ def main():
         choice = input("Enter Choice: ").upper()
 
     print("Thank you for using Items for hire ")
-    file.close()
+    with open("items.csv", "a") as csvfile:
+        wr = csv.writer(csvfile, dialect='excel')
+        wr.write(mylist)
+
 
 ########
 
-def loading_items():
-    """""
+def load_items():
     items = open("items.csv", "r")
-    for line in items.readlines():
-        print("! ", line.strip())
-    items.close()
-"""""
-    items = open("items.csv", "r+")
-    wr = csv.writer(items, dialect='excel')
-
-
     for line in items:
         match = line.strip().split(",")
-        mylist = []
-
         name = match[0]
         description = match[1]
         price = match[2]
         hire = match[3]
         mylist.append((match[0], match[1], match[2], match[3]))
-
-        for thing in mylist:
-            if len(thing[3]) != 3:
-                print(thing[0:3])
     items.close()
-"""
-            for i, lines in enumerate(mylist):
-                print('{} = {}'.format(i, lines))
-                i += 1
 
 
-    items = open("items.csv", "r")
-    reader = csv.reader(items, delimiter="\t")
-    for i, line in enumerate(reader):
-        print('{} = {}'.format(i, line).split())
-    items.close()
-"""
+######
+
+def listing_items():
+    for thing in mylist:
+        if len(thing[3]) != 3:
+            print(thing[0:])
+
+
 #######
 def hiring_item():
-    items = open("items.csv", "r")
-    reader = csv.reader(items, delimiter="\t")
-    for i, line in enumerate(reader):
-        print('{} = {}'.format(i, line))
-    items.close()
+    hire = str(input("Enter the Item you want to hire: "))
+    for i, item_name in enumerate(mylist):
+        if item_name[0] == hire:
+            temp = list(mylist[i])
+            temp[3] = "out"
+            mylist[i] = tuple(temp)
+    for thing in mylist:
+        print(thing[0:])
 
 
+#####
+def return_item():
+    hire = str(input("Enter the Item you want to Return: "))
+    for i, item_name in enumerate(mylist):
+        if item_name[0] == hire:
+            temp = list(mylist[i])
+            temp[3] = "in"
+            mylist[i] = tuple(temp)
+    for thing in mylist:
+        print(thing[0:])
+
+
+#####
 def add_item():
-    new = []
-
     itemName = input("name of item: ")
-    new.append(itemName)
     description = input("enter description: ")
-    new.append(description)
     price = input("enter price: ")
-    new.append(price)
-    new.append('in')
-    print(new)
 
-    with open("output.csv", "a") as csvfile:
-        wr = csv.writer(csvfile, dialect='excel')
-        wr.writerow(new)
+    mylist.append((itemName, description, price, 'in'))
+
+    print(mylist)
+
+    # with open("items.csv", "a") as csvfile:
+    # wr = csv.writer(csvfile, dialect='excel')
+    # wr.writerow(mylist)
+
 
 """
     add = open("items.csv", "a")
     add.write(','.join(new))
     add.close()
 """
-
 main()
